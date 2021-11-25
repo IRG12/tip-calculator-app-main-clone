6450290e886d6663eba145bf2908c72c86df6509
Isaiah G <isaiahrg12@gmail.com>
2021-11-25 15:02:30 -0700

    Made a Function to handle calculation of bill, tip%, and amount of Ppl. Incorporated the calculation to display as well

--- script.js
+++ script.js
@@ -1,82 +1,103 @@
-
-
-
 //----------------------------------------------------------------
 //How do we get the images?
-        let darkMode = false;
-        let themeBtn = document.getElementById("getMoon");
-        let background = document.body.style;
+let darkMode = false;
+let themeBtn = document.getElementById("getMoon");
+let background = document.body.style;
 
+function darkModeF() {
+  // console.log("Dark")
+  themeBtn.src = "./images/icon-sun.svg";
+  //How do we get the background color of the whole HTML page
+  background.backgroundColor = "rgb(20,29,47)";
 
-    function darkModeF() {
-    // console.log("Dark")
-        themeBtn.src = "./images/icon-sun.svg";
-    //How do we get the background color of the whole HTML page
-        background.backgroundColor = 'rgb(20,29,47)';
-        
-        darkMode = true;
-    }
+  darkMode = true;
+}
 //##################################################################
-    function lightModeF() {
-    // console.log("Light")
-        themeBtn.src = "./images/icon-moon.svg";
-    //How do we get the background color of the whole HTML page
-        background.backgroundColor = 'hsl(185, 41%, 84%)';
-        
-        darkMode = false;
-    }
+function lightModeF() {
+  // console.log("Light")
+  themeBtn.src = "./images/icon-moon.svg";
+  //How do we get the background color of the whole HTML page
+  background.backgroundColor = "hsl(185, 41%, 84%)";
+
+  darkMode = false;
+}
 
 //###################################################################
 //How do I make my image clickable?
-    themeBtn.addEventListener('click', function () {
-    
-        if (darkMode == false) {
-
-            darkModeF();
-
-        } else {
-
-            lightModeF();
-        }
-
-    })
+themeBtn.addEventListener("click", function () {
+  if (darkMode == false) {
+    darkModeF();
+  } else {
+    lightModeF();
+  }
+});
 //------------------------------------------------------------------
-
-
-
-// How can we record what input we put in the bill?
-
+let bill = 0;
+let amountOfPpl = 0;
+let tipPerPerson;
+// let totalTip;
+// let totalAmountPerPerson;
+let totalPerPerson;
+let tipPercentage;
+let billInput = document.getElementById("bill-input");
+let people = document.getElementById("pplInput");
+let percent = document.querySelectorAll(".getBtns");
 // How do we get the bill input?
-    let billInput = document.getElementById("bill-input");
-
 // How do we make something run when we put something in the input?
-    billInput.addEventListener("change", function () {
-        
-        console.log(billInput.value)
-    })
-
-
+billInput.addEventListener("change", function () {
+  // console.log(billInput.value);
+  //We got to store "bill" input when functions calls "bill" in it's perimeters. Do the same for the percentage and amountOfPpl SSSSSSOOOOOO...
+  let bill = billInput.value;
+  calSplit(bill, percent, amountOfPpl);
+});
 
 //How do we get access to all the buttons
-    let percent = document.querySelectorAll(".getBtns")
-// console.log(percent)
 //How do we give each one an eventListener?
 // .forEach() is an Array method
-    percent.forEach((btn) => {
-        btn.addEventListener("click", function () {
-            //How do we gain access to the different percent values?
-                console.log(btn.innerText)
-            //How to we change the value from a string to the percent a number value?
-                console.log(parseFloat(btn.innerText)/100)
-        })
-    })
+percent.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    //How do we gain access to the different percent values?
+    // console.log(btn.innerText);
+    //How to we change the value from a string to the percent a number value?
+    // console.log(parseFloat(btn.innerText) / 100);
+    tipPercentage = parseFloat(btn.innerText);
 
+    calSplit(bill, percent, amountOfPpl);
+  });
+});
 
+people.addEventListener("change", function () {
+  // console.log(people.value);
+  let amountOfPpl = people.value;
+  calSplit(bill, percent, amountOfPpl);
+});
 
+const resetCalc = () => {
+  console.log("yoyoyoyoyoy");
+  document.getElementById("tip-amount").innerText = "$0.00";
+  document.getElementById("total-amount").innerText = "$0.00";
+  billInput.value = 0;
+  people.value = 0;
+};
 
+const calSplit = (bill, percentage, amountOfPpl) => {
+  bill = parseInt(billInput.value);
+  percentage = tipPercentage / 100;
+  amountOfPpl = parseFloat(people.value);
 
-let people = document.getElementById("pplInput")
+  console.log(bill);
+  console.log(percentage);
+  console.log(amountOfPpl);
 
-people.addEventListener("change", function() {
-    console.log(people.value)
-})
+  if (bill && percentage && amountOfPpl) {
+    let tipPerPerson = (bill * percentage) / amountOfPpl;
+    console.log(tipPerPerson);
+
+    let totalPerPerson = bill / amountOfPpl + tipPerPerson;
+    console.log(totalPerPerson);
+    document.getElementById("tip-amount").innerText =
+      "$" + tipPerPerson.toFixed(2);
+    document.getElementById("total-amount").innerText =
+      "$" + totalPerPerson.toFixed(2);
+  }
+};
